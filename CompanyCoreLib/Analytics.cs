c91cf42a75fb48a07b31165fe76c7a5fc2c85dd6
Grinsteff <105645233+Grinsteff@users.noreply.github.com>
2023-06-13 21:14:25 +0400

    хуй

---
+++ CompanyCoreLib/Analytics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace CompanyCoreLib
+{
+    public class Analytics
+    {
+        public static List<DateTime> PopularMonths(List<DateTime> purchaseDates)
+        {
+            Dictionary<DateTime, int> monthCounts = new Dictionary<DateTime, int>();
+
+            foreach (DateTime date in purchaseDates)
+            {
+                DateTime month = new DateTime(date.Year, date.Month, 1);
+                if (monthCounts.ContainsKey(month))
+                {
+                    monthCounts[month]++;
+                }
+                else
+                {
+                    monthCounts.Add(month, 1);
+                }
+            }
+
+            var sortedMonths = monthCounts.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+            List<DateTime> popularMonths = new List<DateTime>();
+
+            foreach (KeyValuePair<DateTime, int> pair in sortedMonths)
+            {
+                popularMonths.Add(new DateTime(pair.Key.Year, pair.Key.Month, 1));
+            }
+
+            return popularMonths;
+        }
+        public static void Main()
+        {
+           
+        }
+    }
+    
+}
